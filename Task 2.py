ee3d2a5437b945fc489ee9ab68baff1c1b0d58b2
Affan Yasir <23-CP-24@students.uettaxila.edu.pk>
2025-02-09 13:15:19 +0500

    modified

--- Task 2.py
+++ Task 2.py
@@ -1,7 +1,21 @@
-def calculate_weekly_expenses(expenses):
-    weekly_expenses = sum(expenses[:7])
-    return weekly_expenses
+def calculate_total_expenses(expenses):
+   
+    total = 0
+    for i in range(7):  
+        total += expenses[i]
+    return total
 
 
-daily_expenses = [20.5, 100.75, 50, 30, 15, 25, 40, 60, 10]
-print("Total expenses for the first 7 days:", calculate_weekly_expenses(daily_expenses))
+if __name__ == "__main__":
+   
+    daily_expenses = []
+
+    print("Enter your daily expenses for the last 10 days:")
+    for day in range(10):  
+        expense = float(input(f"Enter expense for Day {day + 1}: "))
+        daily_expenses.append(expense)
+
+    total_expenses = calculate_total_expenses(daily_expenses)
+
+    print(f"\nDaily Expenses: {daily_expenses}")
+    print(f"Total Expenses for the First 7 Days: {total_expenses}")
